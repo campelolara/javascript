467f80ff364c55402c6631fc537aa81464449bfa
Lara <laracampelo505@gmail.com>
2024-08-04 17:11:39 -0300

    js

---
+++ aula13/teste.js
@@ -0,0 +1,24 @@
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
+/* estrutura de repetição com teste lógico no início
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/* estrutura de repetição com teste lógico no final */ 
+
+var c = 1
+do {
+   console.log(`Passo ${c}`) 
+   c++
+} while (c <= 10)
